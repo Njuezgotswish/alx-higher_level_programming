1a7fad0bc9e973165041bc587a11544de2841387
Nk <113770164+Njuezgotswish@users.noreply.github.com>
2023-01-24 19:54:46 +0300

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
+"""square3.py
+"""
 Square = __import__('3-square').Square
 
-my_square_1 = Square(3)
-print("Area: {}".format(my_square_1.area()))
+class Square:
+    """ A class that defines a square by its size
+    """
+    def __init__(self, size=0):
+        """ Method to initialize the square object
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
-try:
-    print(my_square_1.size)
-except Exception as e:
-    print(e)
-
-try:
-    print(my_square_1.__size)
-except Exception as e:
-    print(e)
-
-my_square_2 = Square(5)
-print("Area: {}".format(my_square_2.area()))
+    def area(self):
+        """ Method that returns the square are of the object
+        """
+        return (self.__size ** 2)
