eca52441d119da3f06e6b84fa30e09ec042287eb
Nk <113770164+Njuezgotswish@users.noreply.github.com>
2023-01-24 19:47:30 +0300

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,8 @@
-#!/usr/bin/python3
+#!/usr/bin/python3#!/usr/bin/python3
+# 0-square.py by Ehoneah Obed
+"""A module that defines a square """
+
+
 class Square:
     """ A class that defines a square by its size
     """
