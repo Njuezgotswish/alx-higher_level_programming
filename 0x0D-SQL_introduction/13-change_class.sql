c5f128f121d34c6ffb8cc94732aede68b12d2480
Nk <113770164+Njuezgotswish@users.noreply.github.com>
2023-05-31 14:59:29 +0300

    Update 13-change_class.sql

--- 0x0D-SQL_introduction/13-change_class.sql
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -1,3 +1,4 @@
-cript that removes records
+-- Script that removes records
 -- Query to remove all records with a score <= 5 in the table second_table
+DELETE FROM second_table WHERE score <= 5; to remove all records with a score <= 5 in the table second_table
 DELETE FROM second_table WHERE score <= 5;
